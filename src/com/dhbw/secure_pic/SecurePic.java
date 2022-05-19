4605f84a14de3421c0a54466d2c60a1e8c6f987e
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-19 14:24:17 +0200

    worked on SecurePic

--- src/com/dhbw/secure_pic/SecurePic.java
+++ src/com/dhbw/secure_pic/SecurePic.java
@@ -1,8 +1,17 @@
 package com.dhbw.secure_pic;
 
-// TODO comment + authors
-
+/**
+ * Main entry point to application - providing the main method.
+ *
+ * @author Frederik Wolter
+ */
 public class SecurePic {
+
+    /**
+     * MAIN entry point of application.
+     *
+     * @param args currently not used.
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!"); // TODO
     }
