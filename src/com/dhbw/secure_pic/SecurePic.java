37bb2b2a709fbdf963828558254f0b98560a41d5
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-19 14:17:24 +0200

    worked on project structure

---
+++ src/com/dhbw/secure_pic/SecurePic.java
@@ -0,0 +1,9 @@
+package com.dhbw.secure_pic;
+
+// TODO comment + authors
+
+public class SecurePic {
+    public static void main(String[] args) {
+        System.out.println("Hello world!"); // TODO
+    }
+}
