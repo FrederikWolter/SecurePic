e9375091ec326b516c911c2080e42fb6f05a139a
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-25 18:41:33 +0200

    reworked PlusMinusOne.java to fit class diagramm

--- src/com/dhbw/secure_pic/coder/PlusMinusOne.java
+++ src/com/dhbw/secure_pic/coder/PlusMinusOne.java
@@ -1,5 +1,24 @@
 package com.dhbw.secure_pic.coder;
 
-public class PlusMinusOne {
-    // TODO implement
+import com.dhbw.secure_pic.data.ContainerImage;
+import com.dhbw.secure_pic.data.Information;
+
+// TODO implement
+// TODO comment
+
+public class PlusMinusOne extends Coder{
+
+    public PlusMinusOne(ContainerImage img) {
+        super(img);
+    }
+
+    @Override
+    public ContainerImage encode(Information info) {
+        return null;    // TODO implement
+    }
+
+    @Override
+    public Information decode() {
+        return null;    // TODO implement
+    }
 }
