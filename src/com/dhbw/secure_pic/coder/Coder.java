8864af46daeb82f233e500d38f73689c5c00fcec
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-25 18:31:42 +0200

    reworked Coder.java to fit class diagramm

--- src/com/dhbw/secure_pic/coder/Coder.java
+++ src/com/dhbw/secure_pic/coder/Coder.java
@@ -1,5 +1,24 @@
 package com.dhbw.secure_pic.coder;
+import com.dhbw.secure_pic.data.ContainerImage;
+import com.dhbw.secure_pic.data.Information;
 
-public class Coder {
-    // TODO implement
+// TODO COMMENT
+// TODO implement
+
+public abstract class Coder {
+
+    // region attributes
+    private ContainerImage img;
+    // endregion
+
+
+    public Coder(ContainerImage img){
+        this.img = img;
+    }
+
+
+    public abstract ContainerImage encode(Information info);
+
+    public abstract Information decode();
+
 }
