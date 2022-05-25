7058fd3cabe83a4cfa4f2b4a72a82b2fbfdb40e6
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-25 18:46:15 +0200

    made Crypter abstract

--- src/com/dhbw/secure_pic/crypter/Crypter.java
+++ src/com/dhbw/secure_pic/crypter/Crypter.java
@@ -6,17 +6,14 @@ import com.dhbw.secure_pic.data.Information;
  *  This class implements the Crypter class used to encrypt/decrypt messages
  */
 
-public class Crypter {
+public abstract class Crypter {
 
     /**
      * @param information includes the message to encrypt/decrypt
      * @return
      */
-    private Information encrypt(Information information) {
-        return null;
-    }
+    public abstract Information encrypt(Information information);
 
-    private Information decrypt(Information information) {
-        return null;
-    }
+    public abstract Information decrypt(Information information);
+
 }
