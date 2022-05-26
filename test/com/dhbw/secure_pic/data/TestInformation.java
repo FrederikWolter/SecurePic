0d2174d2b00efe92822c196eb9ef886697c9ff1a
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-26 18:14:49 +0200

    worked on TestInformation.java

--- test/com/dhbw/secure_pic/data/TestInformation.java
+++ test/com/dhbw/secure_pic/data/TestInformation.java
@@ -1,8 +1,11 @@
 package com.dhbw.secure_pic.data;
 
+import com.dhbw.secure_pic.auxiliary.IllegalLengthException;
+import com.dhbw.secure_pic.auxiliary.IllegalTypeException;
 import org.junit.Test;
 import java.nio.charset.StandardCharsets;
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class TestInformation {
 
@@ -11,7 +14,10 @@ public class TestInformation {
         String testString = "Test !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~öäüÖÄÜ€©§¼Äÿ";
 
         Information info =  Information.getInformationFromString(testString);
+
         assertEquals(testString, new String(info.getData(), StandardCharsets.UTF_8));
+        assertEquals(testString.getBytes(StandardCharsets.UTF_8).length, info.getLength());
+        assertEquals(Information.Type.TEXT, info.getType());
     }
 
     @Test
@@ -19,6 +25,26 @@ public class TestInformation {
     }
 
     @Test
-    public void getInformationFromData() {
+    public void getInformationFromData() throws IllegalLengthException, IllegalTypeException {
+        String testString = "Test !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~öäüÖÄÜ€©§¼Äÿ";
+
+        Information info =  Information.getInformationFromString(testString);
+        byte[] rawData = info.toBEBytes();
+
+        Information info2 = Information.getInformationFromData(rawData);
+
+        assertEquals(info.getLength(), info2.getLength());
+        assertEquals(info.getType(), info2.getType());
+        assertArrayEquals(info.getData(), info2.getData());
+    }
+
+    @Test
+    public void toBEBytes() {
+        String testString = "0123456789";
+
+        Information info =  Information.getInformationFromString(testString);
+
+        byte[] result = info.toBEBytes();
+        // TODO add automatic test?
     }
 }
