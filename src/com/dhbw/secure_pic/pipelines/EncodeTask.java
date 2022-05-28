875e111eda17eb0529e763816794c8c213b13752
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-28 21:12:56 +0200

    EncodeTask.java added structure

--- src/com/dhbw/secure_pic/pipelines/EncodeTask.java
+++ src/com/dhbw/secure_pic/pipelines/EncodeTask.java
@@ -1,4 +1,33 @@
 package com.dhbw.secure_pic.pipelines;
 
-public class EncodeTask {
+import javax.swing.*;
+import java.util.List;
+
+
+// TODO comment
+
+/**
+ * @author Thu Giang Tran supported by Frederik Wolter
+ */
+public class EncodeTask extends SwingWorker<ContainerImage, Void> {
+
+    @Override
+    protected ContainerImage doInBackground() throws Exception {
+        return null;
+        // TODO implement
+        // TODO use setProgress(): https://docs.oracle.com/javase/tutorial/uiswing/examples/components/ProgressBarDemoProject/src/components/ProgressBarDemo.java
+    }
+
+    @Override
+    protected void done() {
+        super.done();
+        // TODO implement
+        // TODO error handling: https://stackoverflow.com/a/6524300/13777031
+    }
+
+    /** currently not in use */
+    @Override
+    protected void process(List<Void> chunks) {
+        super.process(chunks);
+    }
 }
