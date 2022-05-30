4b9d3a5afc3a7a32a2bab74d6b44c1043a81a5d3
FeuerS <72504483+FeuerS@users.noreply.github.com>
2022-05-30 23:38:53 +0200

    Asymmetrical Send

--- src/com/dhbw/secure_pic/gui/Send_Asymmetrical.java
+++ src/com/dhbw/secure_pic/gui/Send_Asymmetrical.java
@@ -6,11 +6,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
-public class Send_Symmetrical extends Component {
-    private JPanel MainPanel_SS;
+public class Send_Asymmetrical extends Component {
+    private JPanel MainPanel_SA;
     private JProgressBar progressBar1;
     private JPanel LeftPanel;
-    private JPanel RightPanel;
     private JButton backButton;
     private JRadioButton imageRadioButton;
     private JRadioButton textmessageRadioButton;
@@ -19,34 +18,35 @@ public class Send_Symmetrical extends Component {
     private JComboBox comboBox_EncAlg;
     private JPasswordField passwordField1;
     private JButton encodeButton;
-    private JSlider slider1;
     private JButton uploadButton2;
+    private JPanel RightPanel;
+    private JSlider slider1;
     private JButton uploadButton;
 
     final JFileChooser fc = new JFileChooser();
 
-    public Send_Symmetrical() {
-        uploadButton2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //Handle open button action.
-                if (e.getSource() == uploadButton2) {
-                    int returnVal = fc.showOpenDialog(Send_Symmetrical.this);
-
-                    if (returnVal == JFileChooser.APPROVE_OPTION) {
-                        File file = fc.getSelectedFile();
-                        //This is where a real application would open the file.
-                    } else {
-                    }
-                }
-            }
-        });
+    public Send_Asymmetrical() {
         uploadButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //Handle open button action.
                 if (e.getSource() == uploadButton) {
-                    int returnVal = fc.showOpenDialog(Send_Symmetrical.this);
+                    int returnVal = fc.showOpenDialog(Send_Asymmetrical.this);
+
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        File file = fc.getSelectedFile();
+                        //This is where a real application would open the file.
+                    } else {
+                    }
+                }
+            }
+        });
+        uploadButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Handle open button action.
+                if (e.getSource() == uploadButton2) {
+                    int returnVal = fc.showOpenDialog(Send_Asymmetrical.this);
 
                     if (returnVal == JFileChooser.APPROVE_OPTION) {
                         File file = fc.getSelectedFile();
@@ -57,13 +57,5 @@ public class Send_Symmetrical extends Component {
 
             }
         });
-    }
-    public static void main(String[] args) {
-        JFrame fenster = new JFrame("Image Converter"); //neues Frame bzw. Fenster
-        fenster.setContentPane(new Send_Symmetrical().MainPanel_SS);
-        fenster.setMinimumSize(new Dimension(400,200));
-        fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenster.pack();
-        fenster.setVisible(true);
     }
 }
--- src/com/dhbw/secure_pic/gui/Send_NoEncryption.java
+++ src/com/dhbw/secure_pic/gui/Send_NoEncryption.java
@@ -6,47 +6,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
-public class Send_Symmetrical extends Component {
-    private JPanel MainPanel_SS;
-    private JProgressBar progressBar1;
+public class Send_NoEncryption extends Component {
+    private JPanel MainPanel_SN;
     private JPanel LeftPanel;
-    private JPanel RightPanel;
     private JButton backButton;
     private JRadioButton imageRadioButton;
     private JRadioButton textmessageRadioButton;
     private JTextArea Message;
     private JComboBox comboBox_CodAlg;
-    private JComboBox comboBox_EncAlg;
-    private JPasswordField passwordField1;
     private JButton encodeButton;
-    private JSlider slider1;
     private JButton uploadButton2;
+    private JPanel RightPanel;
+    private JSlider slider1;
     private JButton uploadButton;
+    private JProgressBar progressBar1;
 
     final JFileChooser fc = new JFileChooser();
 
-    public Send_Symmetrical() {
-        uploadButton2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //Handle open button action.
-                if (e.getSource() == uploadButton2) {
-                    int returnVal = fc.showOpenDialog(Send_Symmetrical.this);
-
-                    if (returnVal == JFileChooser.APPROVE_OPTION) {
-                        File file = fc.getSelectedFile();
-                        //This is where a real application would open the file.
-                    } else {
-                    }
-                }
-            }
-        });
+    public Send_NoEncryption() {
         uploadButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //Handle open button action.
                 if (e.getSource() == uploadButton) {
-                    int returnVal = fc.showOpenDialog(Send_Symmetrical.this);
+                    int returnVal = fc.showOpenDialog(Send_NoEncryption.this);
 
                     if (returnVal == JFileChooser.APPROVE_OPTION) {
                         File file = fc.getSelectedFile();
@@ -57,13 +40,21 @@ public class Send_Symmetrical extends Component {
 
             }
         });
-    }
-    public static void main(String[] args) {
-        JFrame fenster = new JFrame("Image Converter"); //neues Frame bzw. Fenster
-        fenster.setContentPane(new Send_Symmetrical().MainPanel_SS);
-        fenster.setMinimumSize(new Dimension(400,200));
-        fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenster.pack();
-        fenster.setVisible(true);
+        uploadButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Handle open button action.
+                if (e.getSource() == uploadButton2) {
+                    int returnVal = fc.showOpenDialog(Send_NoEncryption.this);
+
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        File file = fc.getSelectedFile();
+                        //This is where a real application would open the file.
+                    } else {
+                    }
+                }
+
+            }
+        });
     }
 }
