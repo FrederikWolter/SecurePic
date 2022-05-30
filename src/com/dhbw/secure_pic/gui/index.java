8a2a08a35e77af7079aa3c48cc280e749148f955
FeuerS <72504483+FeuerS@users.noreply.github.com>
2022-05-30 14:49:21 +0200

    FileSelectFenster

--- src/com/dhbw/secure_pic/gui/index.java
+++ src/com/dhbw/secure_pic/gui/index.java
@@ -1,10 +1,13 @@
 package com.dhbw.secure_pic.gui;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
-public class index {
+
+public class index extends Component {
     private JPanel MainPanelImgCon;
     private JButton buttonStartApp;
     private JProgressBar progressBar1;
@@ -15,8 +18,11 @@ public class index {
     private JRadioButton radioButton5;
     private JRadioButton radioButton6;
     private JButton button2;
+    private JButton uploadButton;
 
     public index() {
+        final JFileChooser fc = new JFileChooser();
+
         buttonStartApp.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -24,12 +30,27 @@ public class index {
 
             }
         });
+        uploadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Handle open button action.
+                if (e.getSource() == uploadButton) {
+                    int returnVal = fc.showOpenDialog(index.this);
+
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        File file = fc.getSelectedFile();
+                        //This is where a real application would open the file.
+                    } else {
+                    }
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
         JFrame fenster = new JFrame("Image Converter"); //neues Frame bzw. Fenster
         fenster.setContentPane(new index().MainPanelImgCon);
-        //fenster.setSize(600, 400);
+        fenster.setMinimumSize(new Dimension(400,200));
         fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fenster.pack();
         fenster.setVisible(true);
