2b4595190760b4f500ddfebca030ac2832a815e1
kngkiro <kirolis2002@gmail.com>
2022-06-05 21:11:37 +0200

    TestAES cleanup

--- test/com/dhbw/secure_pic/crypter/TestAES.java
+++ test/com/dhbw/secure_pic/crypter/TestAES.java
@@ -3,13 +3,13 @@ package com.dhbw.secure_pic.crypter;
 import com.dhbw.secure_pic.data.Information;
 import org.junit.Test;
 
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidAlgorithmParameterException;
+
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -24,18 +24,18 @@ import static org.junit.Assert.assertNotEquals;
 public class TestAES {
 
     @Test
-    public void testAESEncryptDecrypt() throws  NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public void testAESEncryptDecrypt() throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
 
         String message = "Testing «ταБЬℓσ»: 1<2 & 4+1>3, now 20% off!";
         Information information = Information.getInformationFromString(message);
 
-        AES encryptAES = new AES("password");
+        AES encryptAES = new AES("password125444/&&§");
 
         encryptAES.encrypt(information);
         assertNotEquals(information.toText(), message);
 
         //A second AES is created to simulate two different devices
-        AES decryptAES = new AES("password");
+        AES decryptAES = new AES("password125444/&&§");
         decryptAES.decrypt(information);
         assertEquals(information.toText(), message);
     }
