1d958aeaa7f3b692e0d728680d44c4fb35fd1fc9
FeuerS <72504483+FeuerS@users.noreply.github.com>
2022-06-07 23:05:18 +0200

    Mini Anpassungen

--- src/com/dhbw/secure_pic/gui/StartChooseType.java
+++ src/com/dhbw/secure_pic/gui/StartChooseType.java
@@ -1,6 +1,8 @@
 package com.dhbw.secure_pic.gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 // TODO comment (normal comments + JDocs) # only delete if final#
 
@@ -11,6 +13,16 @@ public class StartChooseType {
     private JButton sendButton;
     private JButton receiveButton;
     private JPanel MainPanel_ST;
+    private JComboBox comboBox1;
+
+    public StartChooseType() {
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 
     public JButton getCloseButton() {
         return closeButton;
