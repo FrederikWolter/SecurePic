d056c26ffebb1f1d327ab6dd7b600b3c916a3ab1
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-06-13 17:01:25 +0200

    worked on ReceiveSymmetrical

--- src/com/dhbw/secure_pic/gui/ReceiveSymmetrical.java
+++ src/com/dhbw/secure_pic/gui/ReceiveSymmetrical.java
@@ -1,7 +1,21 @@
 package com.dhbw.secure_pic.gui;
 
+import com.dhbw.secure_pic.coder.Coder;
+import com.dhbw.secure_pic.coder.LeastSignificantBit;
+import com.dhbw.secure_pic.coder.PlusMinusOne;
+import com.dhbw.secure_pic.crypter.AES;
+import com.dhbw.secure_pic.crypter.Crypter;
+import com.dhbw.secure_pic.crypter.EmptyCrypter;
+import com.dhbw.secure_pic.data.ContainerImage;
+import com.dhbw.secure_pic.data.Information;
+import com.dhbw.secure_pic.gui.utility.DecodeFinishedHandler;
 import com.dhbw.secure_pic.gui.utility.FileSelect;
+import com.dhbw.secure_pic.gui.utility.LoadFinishedHandler;
+import com.dhbw.secure_pic.gui.utility.SaveSelect;
+import com.dhbw.secure_pic.pipelines.ContainerImageLoadTask;
+import com.dhbw.secure_pic.pipelines.DecodeTask;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.DataFlavor;
@@ -13,10 +27,16 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
+import java.io.IOException;
+
+import static javax.swing.JOptionPane.WARNING_MESSAGE;
 
 // FIXME comment (normal comments + JDocs) # only delete if final#
 
+// TODO show text not in label but Textarea for automatic line breaks!
+
 public class ReceiveSymmetrical extends Component {
+    // region swing attributes
     private JProgressBar progressBar;
     private JButton backButton;
     private JButton decodeButton;
@@ -30,34 +50,75 @@ public class ReceiveSymmetrical extends Component {
     private JPanel uploadPanelContainer;
     private JLabel containerImg;
     private JComboBox codeComboBox;
+    // endregion
 
-    final FileSelect fs = new FileSelect();
+    // region attributes
+    private transient ContainerImage containerImage;
+    private transient Information contentInformation;
+
+    private final int containerImageDisplayHeight = 200;
+    private final int containerImageDisplayWidth = 200;
+    private final int messageImageDisplayHeight = 550;
+    private final int messageImageDisplayWidth = 550;
+    // endregion
 
     public ReceiveSymmetrical(Gui parent) {
-        uploadPanelContainer.setDropTarget(new DropTarget() {
+
+        PropertyChangeListener propertyChangeListener = new PropertyChangeListener() {
             @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                if ("progress".equals(evt.getPropertyName())) {
+                    int progress = (Integer) evt.getNewValue();
+                    progressBar.setValue(progress);
+                }
+            }
+        };
+
+        LoadFinishedHandler finishedContainerImageLoad = new LoadFinishedHandler() {
+            @Override
+            public void finishedContainerImageLoad(ContainerImage image) {
+                containerImage = image;
+
+                containerImg.setText("");
+                containerImg.setIcon(new ImageIcon(Gui.getScaledImage(containerImage.getImage(),
+                        containerImageDisplayWidth,
+                        containerImageDisplayHeight)));
+            }
+        };
+
+        uploadPanelContainer.setDropTarget(new DropTarget() {
             public synchronized void drop(DropTargetDropEvent evt) {
                 try {
                     evt.acceptDrop(DnDConstants.ACTION_COPY);
                     java.util.List<File> droppedFiles = (java.util.List<File>) evt.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);    // FIXME cleanup cast?
 
-                    for (File file : droppedFiles) { // TODO allow multiple files? no? GENERAL
-
-                        //ToDo noch anppasen
-
+                    for (File file : droppedFiles) { // FIXME allow multiple files? no? GENERAL
+                        ContainerImageLoadTask task = new ContainerImageLoadTask(file.getPath(), finishedContainerImageLoad);
+                        task.addPropertyChangeListener(propertyChangeListener);
+                        task.execute();
                     }
-                } catch (Exception ex) {    // TODO error handling?
+                } catch (Exception ex) {    // FIXME error handling?
                     ex.printStackTrace();
                 }
                 decodeButton.setEnabled(true);
             }
         });
 
+        // region listener
         uploadContainerImg.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                File file = new FileSelect().selectFile(ReceiveSymmetrical.this);
 
-                File file = fs.selectFile(ReceiveSymmetrical.this);
+                if(file == null){
+                    return;
+                }
+
+                ContainerImageLoadTask task = new ContainerImageLoadTask(file.getPath(), finishedContainerImageLoad);
+                task.addPropertyChangeListener(propertyChangeListener);
+                task.execute();
+
+                decodeButton.setEnabled(true);
             }
         });
 
@@ -68,55 +129,114 @@ public class ReceiveSymmetrical extends Component {
             }
         });
 
-        uploadContainerImg.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //ToDO Uploadfunction
-
-                decodeButton.setEnabled(true);
-            }
-        });
         decodeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                decodeButton.setEnabled(false);
-                //ToDo Start und Überprüfung encodeTask
 
-                exportButton.setEnabled(true);
-                copyToClipboardButton.setEnabled(true);
+                Coder coder;
+                Crypter crypter;
+
+                if (containerImage == null){
+                    // FIXME error handling
+                    return;
+                }
+
+                if (codeComboBox.getSelectedItem() == "LSB"){
+                    coder = new LeastSignificantBit(containerImage);
+                } else if(codeComboBox.getSelectedItem() == "PM1"){
+                    coder = new PlusMinusOne(containerImage);
+                } else {
+                    // FIXME error handling
+                    return;
+                }
+
+                if (encryptComboBox.getSelectedItem() == "AES"){
+                    String password = new String(passwordField.getPassword());
+                    if(password.length() > 0){
+                        crypter = new AES(password);
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Bitte gebe ein Passwort ein, mit dem die Information entschlüsselt werden soll.", "Warnung", WARNING_MESSAGE);
+                        return;
+                    }
+                } else {
+                    // TODO error handling
+                    return;
+                }
+
+//                decodeButton.setEnabled(false);
+
+                DecodeTask task = new DecodeTask(coder, crypter, new DecodeFinishedHandler() {
+                    @Override
+                    public void finishedDecode(Information info) {
+                        contentInformation = info;
+
+                        Information.Type type = info.getType();
+                        if (type == Information.Type.TEXT) {
+                            messageOutput.setText(info.toText());
+                        } else if (type == Information.Type.IMAGE_PNG || type == Information.Type.IMAGE_GIF || type == Information.Type.IMAGE_JPG){
+                            exportButton.setEnabled(true);
+                            try{
+                                messageOutput.setText("");
+                                messageOutput.setIcon(new ImageIcon(Gui.getScaledImage(info.toImage(),
+                                        messageImageDisplayWidth,
+                                        messageImageDisplayHeight)));
+                            }catch (IOException e){
+                                System.out.println(e);
+                                // FIXME error handling?
+                            }
+                        } else {
+                            // FIXME error handling?
+                        }
+
+                        copyToClipboardButton.setEnabled(true);
+                        decodeButton.setEnabled(true);
+                    }
+                });
+                task.addPropertyChangeListener(propertyChangeListener);
+                task.execute();
             }
         });
+
         exportButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //ToDo export Funktion
+                File file = new SaveSelect().selectDir(ReceiveSymmetrical.this);
 
-            }
-        });
-        copyToClipboardButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //ToDO copy to Clipboard
+                if(file == null){
+                    return;
+                }
 
-            }
-        });
-        progressBar.addPropertyChangeListener(new PropertyChangeListener() {
-            //ToDo Progress anbinden
-            @Override
-            public void propertyChange(PropertyChangeEvent evt) {
-                if ("progress" == evt.getPropertyName()) {
-                    int progress = (Integer) evt.getNewValue();
-                    progressBar.setValue(progress);
+                if(contentInformation.getType() == Information.Type.TEXT)
+                    return;
+
+                try {
+                    ImageIO.write(contentInformation.toImage(), "png", file);
+                    JOptionPane.showMessageDialog(null, "Das decodierte Bild wurde erfolgreich exportiert.", "Erfolg",  JOptionPane.INFORMATION_MESSAGE);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex); // FIXME error handling
                 }
             }
         });
+
+        copyToClipboardButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    contentInformation.copyToClipboard();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                    // FIXME error handing?
+                }
+            }
+        });
+
+        // endregion
     }
 
+    // region getter
     public JPanel getContentPane() {
         return contentPane;
     }
+    // endregion
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
 }
