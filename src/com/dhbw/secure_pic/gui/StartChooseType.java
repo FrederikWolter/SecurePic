c1395bd4b274f1fd81a83c672e2cf98a167924ca
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-07-08 13:05:00 +0200

    worked on StartChooseType

--- src/com/dhbw/secure_pic/gui/StartChooseType.java
+++ src/com/dhbw/secure_pic/gui/StartChooseType.java
@@ -1,53 +1,53 @@
 package com.dhbw.secure_pic.gui;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-// TODO comment (normal comments + JDocs) # only delete if final#
+
 /**
- * Class for the Choose-Type window. In this window one can choose between the send-Function and the receive-Function.
- * The structure and the components are managed in the StartChooseType.form
+ * Class representing Choose-Type {@link GuiView}.<br>
+ * User can choose between send and receive functionality. The layout of the view is defined in 'StartChooseType.form'.
  *
- * @author Hassan El-Khalil
+ * @author Hassan El-Khalil, Kai Schwab, Frederik Wolter
  */
 public class StartChooseType extends GuiView {
+
+    // region swing attributes
     private JButton backButton;
     private JButton sendButton;
     private JButton receiveButton;
     private JPanel contentPane;
-    private JTextPane sendDesc;
-    private JTextPane receiveDesc;
+    // endregion
 
+    /**
+     * Constructor of {@link StartChooseType}.
+     * @param parent parent Gui object
+     */
     public StartChooseType(Gui parent) {
-        // Set an ActionListener for each Button sending the user to the corresponding window
-        backButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                parent.showView(Gui.View.IMAGE_CONVERTER);
-            }
+        // region listener
+        backButton.addActionListener(e ->
+            parent.showView(Gui.View.IMAGE_CONVERTER)
+        );
+
+        receiveButton.addActionListener(e -> {
+            parent.setType(Gui.Type.RECEIVE);   // save chosen type in gui
+            parent.showView(Gui.View.START_CHOOSE_ENCRYPTION);
         });
 
-        receiveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                parent.setType(Gui.Type.RECEIVE);
-                parent.showView(Gui.View.START_CHOOSE_ENCRYPTION);
-            }
+        sendButton.addActionListener(e -> {
+            parent.setType(Gui.Type.SEND);      // save chosen type in gui
+            parent.showView(Gui.View.START_CHOOSE_ENCRYPTION);
         });
-
-        sendButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                parent.setType(Gui.Type.SEND);
-                parent.showView(Gui.View.START_CHOOSE_ENCRYPTION);
-            }
-        });
+        // endregion
     }
 
     // region getter
+
+    /**
+     * @return ContentPane
+     */
     public JPanel getContentPane() {
         return contentPane;
     }
+
     // endregion
 }
